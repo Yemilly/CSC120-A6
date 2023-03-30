440734252b93c268bf61b4a5785a76307806e0e9
Yemilly Soto <ysoto@smith.edu>
2023-03-29 22:00:29 -0400

    hw 6

--- Library.java
+++ Library.java
@@ -1,12 +1,96 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
+/* This is a stub for the Library class */
+public class Library extends Building{
+
+  private Hashtable<String,
+  Boolean> collection = new Hashtable<String,Boolean>();
   
+  /** library class:
+   * @param name
+   * @param address
+   * @param nFloors
+  */
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+    
+      //System.out.println("You have built a library: 📖");
+    }
+  /** add book:
+   * @param title
+   * @return
+   */
+    public void addTitle(String title){
+      this.collection.put(title, true);
+
+
+    }
+
+
+    /** remove book:
+     * @param title
+     * @return
+     */
+    public String removeTitle(String title){
+      this.collection.remove(title);
+      return title;
+      
+    } // return the title that we removed
+
+    /**check out book:
+     * @param title
+     */
+    public void checkOut(String title){
+      this.collection.replace(title, true, false);
+
+    }
+    
+  /**return book:
+   * @param title
+   */
+    public void returnBook(String title){
+      this.collection.replace(title, false, true);
+      
+    }
+
+
+    /** check if title appears as a key in the collection:
+     * @param title
+     * @return
+     */
+    public boolean containsTitle (String title){// returns true if the title appears as a key in the Libary's collection, false otherwise
+      return this.collection.containsKey(title);
+    }
+
+    /**returns true if the title is currently available, false otherwise
+     * @param title
+     * @return
+     */
+    public boolean isAvailable(String title){
+      return this.collection.get(title);
+
+    }
+    /**prints out the entire collection in an easy-to-read way (including checkout status)
+
+     */
+     public void printCollection(){
+
+
+    }
+
+
+
     public static void main(String[] args) {
-      new Library();
+      Library neilson = new Library("neilson", "7 Neilson Drive",4);
+      neilson.addTitle("The Lorax by Dr. Seuss");
+      neilson.addTitle("Mexican Gothic");
+      neilson.checkOut("Mexican Gothic");
+      neilson.returnBook("Mexican Gothic");
+
+
+
+
     }
   
   }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,8 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+    * my initial impression of inherintance was that it was pretty helpful when creating the other classes, having the building parent class. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    * I had a lot of trouble trying to figure out the hashtable, and I kept having trouble trying to figure out my bug for the cafe values not being used. it was definitely tricky but playing around with my code and with the examples from class really helped figure out my next steps overall.
+
